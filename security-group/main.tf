ecdfdb5bd8c909a939d6efb39bba32993285f07e
ifediM <kingsley.maduekwe1@gmail.com>
2026-03-31 16:09:45 +0100

    network edit

--- security-group/main.tf
+++ security-group/main.tf
@@ -1,14 +1,14 @@
 # create security group for the app server
 resource "aws_security_group" "app_server_security_group" {
   name        = "${var.project_name}-${var.environment}-app-server-sg"
-  description = "enable http/https access on port 80/443 via alb sg"
+  description = "enable ssh/https access on port 22/443 via alb sg"
   vpc_id      = var.vpc_id
 
   ingress {
-    description     = "http access"
-    from_port       = 80
-    to_port         = 80
-    protocol        = "tcp"
+    description     = "ssh access"
+    from_port       = 22
+    to_port         = 22
+    protocol        = "ssh"
     cidr_blocks     = ["${var.vpc_cidr}"]
   }
 
