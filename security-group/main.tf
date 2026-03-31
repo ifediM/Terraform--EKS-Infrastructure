c580eb5f6a19a2dfa2b4769f002643809d676a60
ifediM <kingsley.maduekwe1@gmail.com>
2026-03-31 18:14:15 +0100

    sg

--- security-group/main.tf
+++ security-group/main.tf
@@ -5,11 +5,11 @@ resource "aws_security_group" "app_server_security_group" {
   vpc_id      = var.vpc_id
 
   ingress {
-    description     = "ssh access"
-    from_port       = 22
-    to_port         = 22
+    description     = "http access"
+    from_port       = 80
+    to_port         = 80
     protocol        = "tcp"
-    cidr_blocks     = ["${var.vpc_cidr}"]
+    security_groups = [aws_security_group.alb_sg.id]
   }
 
   ingress {
@@ -17,7 +17,16 @@ resource "aws_security_group" "app_server_security_group" {
     from_port       = 443
     to_port         = 443
     protocol        = "tcp"
-    cidr_blocks     = ["${var.vpc_cidr}"]
+    cidr_blocks     = [aws_security_group.alb_sg.id]
+  }
+
+  # Add this INSIDE your app_server_security_group resource
+  ingress {
+    description     = "Allow SSH from EICE"
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.eice_security_group.id]
   }
 
   egress {
@@ -29,5 +38,60 @@ resource "aws_security_group" "app_server_security_group" {
 
   tags = {
     Name = "${var.project_name}-${var.environment}-app-server-sg"
+  }
+}
+
+resource "aws_security_group" "alb_sg" {
+  name        = "${var.project_name}-${var.environment}-alb-sg"
+  description = "enable alb expose the app access via alb sg"
+  vpc_id = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow everyone to hit the redirect
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow everyone to see the site
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "eice_security_group" {
+  name        = "${var.project_name}-${var.environment}-eice-sg"
+  description = "Security group for EC2 Instance Connect Endpoint"
+  vpc_id      = var.vpc_id
+
+  # Ingress: Allow SSH traffic from the internet to the Endpoint
+  ingress {
+    description = "SSH from the internet to EICE"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # This is safe because EICE requires IAM auth
+  }
+
+  # Egress: Allow the Endpoint to send SSH traffic to your App Servers
+  egress {
+    description     = "Allow EICE to talk to App Servers"
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    security_groups = [aws_security_group.app_server_security_group.id]
+  }
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-eice-sg"
   }
 }
